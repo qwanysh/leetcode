812d00566cb14251c03f0edbbf76fbec61abbf09
qwanysh <olimpsdu@gmail.com>
2021-12-13 01:18:44 +0600

    55. Jump Game

---
+++ medium/55.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
+
+        for i in range(len(nums) - 2, -1, -1):
+            if goal - i <= nums[i]:
+                goal = i
+
+        return goal == 0
